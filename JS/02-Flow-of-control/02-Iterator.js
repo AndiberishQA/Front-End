abb08aa91afe29163b660e4ba99f882cb3c7baf0
savannahvaith <sav.vaithilingam@gmail.com>
2021-01-27 10:15:46 +0000

    SV: Moved ASI to Fundamentals

--- JS/02-Flow-of-control/02-Iterator.js
+++ JS/02-Flow-of-control/02-Iterator.js
@@ -17,39 +17,3 @@ for(let i = 0; i<=3;){
 for(let j =0; j<=3;){
     console.log(`j = ${j++}`);
 }
-
-
-// ASI - Automatic semi colon Insertion 
-// 1. Adds a ; when two lines are seperated by a enter key
-// 2. When two statements are separated by a }
-// 3. Line terminator followed by a BREAK, CONTINUE, RETURN / THROWS.
-
-let greeting = "Hello friends"
-console.log(greeting)
-
-let foo
-bar;
-
-// INTERPRETED as: let foo; bar; 
-
-console.log(foo); // foo
-console.log(bar); // nothing, not been declared
-
-let a 
-b 
-= 3
-
-// INTERPRETED as expected: let a, b=3; 
-
-console.log(a); // undefined
-console.log(b); // error?, 3, 
-
-return
-2*b+1
-
-/* Interpreted as: 
-
-return; 
-2*b+1; 
-
-*/
