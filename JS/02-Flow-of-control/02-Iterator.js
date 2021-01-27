341bec0d5d4da49bb59bbad62296ea012a1b703e
savannahvaith <sav.vaithilingam@gmail.com>
2021-01-27 11:21:56 +0000

    SV: Added Loops and Ternary IF

--- JS/02-Flow-of-control/02-Iterator.js
+++ JS/02-Flow-of-control/02-Iterator.js
@@ -17,3 +17,71 @@ for(let i = 0; i<=3;){
 for(let j =0; j<=3;){
     console.log(`j = ${j++}`);
 }
+
+
+// for ( [initialexpress]; [conditon]; iteration){
+//     statement
+// }
+
+for( let i=0; i<=3; i++){
+    console.log(i);
+};
+
+// do while & while 
+
+// do while => will always run ATLEAST once before a checking a condition
+let canirun = false; 
+do{
+    console.log(`I run atleast once!`)
+}while(canirun);
+
+// while => checks the condition first, if :( then it wont run!
+
+let pups = 0; 
+let enough = false; 
+
+while(enough){
+    console.log(`Look a puppyyy!`);
+    pups++;
+
+    if(pups >5){
+        enough = false;
+    }
+}
+
+// switch case = (evaluates an expression, then finds matching case)
+
+let now = new Date(); 
+
+switch (now.getDay()){
+    case 0:
+        console.log(`sunday`);
+        break; 
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log(`It's a weekday`);
+        break;
+    case 6:
+        console.log(`It's saturday`);
+    default:
+        console.log(`Don't know the day...`);
+        break;
+}
+
+let greeting = `good `;
+
+if(now.getHours() > 17){
+    greeting += `evening`;
+}else{
+    greeting += `day`;
+}
+
+console.log(greeting);
+
+Syntax: (condition) ? valueIfTrue : valueIfFalse;
+
+let greeting2 = `good ` + ((now.getHours()>17) ? `evening` : `day`);
+console.log(greeting2);
